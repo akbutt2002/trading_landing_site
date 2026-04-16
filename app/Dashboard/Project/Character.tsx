54b0d5c1dc03aa4af6c05939a9ec8a90475246d2
Muhammad Ahmad <153552341+akbutt2002@users.noreply.github.com>
2026-04-16 16:33:23 +0500

    Initialize ref with null in PokemonModel component

--- app/Dashboard/Project/Character.tsx
+++ app/Dashboard/Project/Character.tsx
@@ -6,7 +6,7 @@ import { useRef } from "react";
 
 export default function PokemonModel({ mouse }: any) {
   const { scene } = useGLTF("/charizard.glb");
-  const ref = useRef<any>();
+  const ref = useRef<any>(null);
 
   useFrame(() => {
     if (!ref.current) return;
